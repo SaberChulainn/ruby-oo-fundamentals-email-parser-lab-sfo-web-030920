a59667eb76d488944ebff7f4f873eec7a2dffef6
Ty Bandachit <tbandachit@gmail.com>
2020-03-10 23:38:53 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ class EmailAddressParser
   attr_accessor :email_addresses
   def initialize(email)
     @email_addresses = []
-    @email_addresses.push(email)
+    @email_addresses.push(email.split())
   end
   
   def parse
