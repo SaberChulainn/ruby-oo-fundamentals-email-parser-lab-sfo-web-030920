3989e8dd9ed0a1b105b1d01e9b6f5a041e2e26c7
Ty Bandachit <tbandachit@gmail.com>
2020-03-11 00:13:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
   end
   
   def parse
-    @email_addresses.each{|string| puts string.tr("/,(\s+)?/", '')}
+    @email_addresses.each {|string| puts string.tr("/,(\s+)?/", '')}
     #@email_addresses.split()
     binding.pry
     puts "wtf"
